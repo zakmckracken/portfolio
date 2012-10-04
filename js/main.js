1564c3b162f452bb8a37203b201691634c69753a
Nikolaus Kinder <largolagrande@gmx.net>
2012-10-04 19:23:09 +0200

    created index, contact and disclaimers

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Author:
+Juliane Doehler
+04.10.2012
+*/
+
+var height = $(document).height() - 90;
+var content_height = $(document).height() - 121;
+$("#main").css("min-height", height);
+$("#content").css("min-height", content_height);
+$("#content_text").css("min-height", content_height);
+
