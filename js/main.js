8bd36567c845d5c1dbfad45189fafc385c567a43
Nikolaus Kinder <largolagrande@gmx.net>
2012-10-07 14:36:28 +0200

    fixed font face for ff and added CNAME

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ Juliane Doehler
 */
 
 var height = $(document).height() - 90;
-var content_height = $(document).height() - 121;
+var content_height = $(document).height() - 122;
 $("#main").css("min-height", height);
 $("#content").css("min-height", content_height);
 $("#content_text").css("min-height", content_height);
