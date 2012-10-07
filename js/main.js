d871e85f5a582632245197c99b360b496fbdabd6
Nikolaus Kinder <largolagrande@gmx.net>
2012-10-07 14:39:19 +0200

    first pages commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* Author:
+Juliane Doehler
+04.10.2012
+*/
+
+var height = $(document).height() - 90;
+var content_height = $(document).height() - 122;
+$("#main").css("min-height", height);
+$("#content").css("min-height", content_height);
+$("#content_text").css("min-height", content_height);
+
